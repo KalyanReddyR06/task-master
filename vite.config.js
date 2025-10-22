03e7c91c5fefee017fc2c30ede647fcf9549c92f
KalyanReddyR06 <rksthor@gamil.com>
2025-10-22 23:35:55 +0530

    Add GitHub Pages deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/TaskMaster/',
+  base: '/task-master/',   
   plugins: [react()],
 })
